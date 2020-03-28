00b3d866457d3237f15768a3ea23aa03d6fb9a54
Weverton Icaro <vertin_xd@hotmail.com>
2020-03-27 21:43:58 -0300

    Exercise 06
    Template Literals

---
+++ exercicio06.js
@@ -0,0 +1,6 @@
+// 06
+// Converta o seguinte trecho de código utilizando Template Literals:
+const usuario = 'Diego';
+const idade = 23;
+console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+console.log(`O usuário ${usuario} possui ${idade} anos.`);
